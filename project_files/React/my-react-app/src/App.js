186093a251ff05cc6885d4a3574d1d1b06644b77
Manasadanda1234 <manasadanda5@gmail.com>
2024-11-23 12:29:29 +0530

    Project_Details

---
+++ project_files/React/my-react-app/src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+    <div className='App1'>
+      <h1 className='Heading'>`Vivo`</h1>
+        <p>30000</p>
+        <p>40%</p>
+    </div>
+    <div className='App1'>
+      <h1 className='Heading'>`Samsung`</h1>
+        <p>40000</p>
+        <p>30%</p>
+    </div>
+    <div className='App1'>
+      <h1  className='Heading'>`Redmi`</h1>
+        <p>50000</p>
+        <p>20%</p>
+    </div>
+</div>
+
+  );
+}
+
+
+export default App;
